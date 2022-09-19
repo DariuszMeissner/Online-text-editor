10c88fec4e2a57e0f782b81aa8e7c4a564607daf
DariuszMeissner <dariusz.r.meissner@gmail.com>
2022-09-19 14:32:05 +0200

    remove jsPDF package, adding rule for printing, used window.print() as save to pdf

--- src/component/ui/print-to-pdf/PrintToPdf.tsx
+++ src/component/ui/print-to-pdf/PrintToPdf.tsx
@@ -1,24 +1,12 @@
 import React, { FC } from 'react'
-import jsPDF from 'jspdf'
 import Button from '../button/Button'
 
 const PrintToPdf: FC = () => {
   const generatePDF = () => {
-    // eslint-disable-next-line new-cap
-    const createDocument = new jsPDF('p', 'mm', 'a4')
-    const elementToPDF = window.document.getElementById('workspace') as HTMLElement
-    createDocument.html(elementToPDF, {
-      callback: pdf => {
-        pdf.save('pdf')
-      }
-    })
+    window.print()
   }
 
-  return (
-    <div>
-      <Button onClick={generatePDF} classValue="btn-primary" icon="Print to pdf" />
-    </div>
-  )
+  return <Button onClick={generatePDF} classValue="btn-primary" icon="Save as pdf" />
 }
 
 export default PrintToPdf
