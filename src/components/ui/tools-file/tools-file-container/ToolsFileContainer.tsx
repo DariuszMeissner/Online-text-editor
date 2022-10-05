044996bc35d154036eade873ff48957a352d5c65
DariuszMeissner <dariusz.r.meissner@gmail.com>
2022-10-05 11:43:26 +0200

    adding name of editor

--- src/components/ui/tools-file/tools-file-container/ToolsFileContainer.tsx
+++ src/components/ui/tools-file/tools-file-container/ToolsFileContainer.tsx
@@ -1,6 +1,25 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import React, { FC } from 'react'
 import Menu from '../../../menu/Menu'
 
-const ToolsFileContainer: FC = () => <Menu />
+const ToolsFileContainer: FC = () => {
+  return (
+    <div>
+      <div style={style.editorName}>Online text editor</div>
+      <Menu />
+    </div>
+  )
+}
+
+const style = {
+  editorName: {
+    fontSize: '12px',
+    textAlign: 'center',
+    paddingBottom: '4px',
+    paddingTop: '4px',
+    borderBottom: '1px solid #F5F5F5',
+    color: '#7B7B7B'
+  }
+} as const
 
 export default ToolsFileContainer
